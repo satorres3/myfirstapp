5bf632d34a1b2f80e2a0796a52fd21ed9cd94878
satorres3 <sa@plazz.ag>
2025-08-16 20:59:52 +0200

    Redirect anonymous users to login

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,27 +1,30 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
-@login_required
 def hub_view(request):
     """Render the hub page."""
+    if not request.user.is_authenticated:
+        return redirect('login')
     return render(request, 'dashboard/hub.html')
 
 
-@login_required
 def settings_view(request):
     """Render the settings page."""
+    if not request.user.is_authenticated:
+        return redirect('login')
     return render(request, 'dashboard/settings.html')
 
 
-@login_required
 def settings_detail_view(request, container_id):
     """Render the settings detail page for a container."""
+    if not request.user.is_authenticated:
+        return redirect('login')
     context = {"container_id": container_id}
     return render(request, 'dashboard/settings_detail.html', context)
 
 
-@login_required
 def container_view(request, container_id):
     """Render the container chat page."""
+    if not request.user.is_authenticated:
+        return redirect('login')
     context = {"container_id": container_id}
     return render(request, 'dashboard/container.html', context)
