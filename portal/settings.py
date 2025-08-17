eb03f6dd706af36f0acfc155517ff47761ee6445
satorres3 <sa@plazz.ag>
2025-08-17 11:37:26 +0200

    Fix: replace os.path with pathlib

--- portal/settings.py
+++ portal/settings.py
@@ -1,7 +1,6 @@
 
 
 
-import os
 import json
 import logging
 from pathlib import Path
@@ -16,7 +15,7 @@ env = environ.Env()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(BASE_DIR / '.env')
 
 
 class JsonFormatter(logging.Formatter):
